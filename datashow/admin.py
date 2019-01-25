68be17b73cbedba59a90dad298f68d0f00183cba
weber19911101 <2217994485@qq.com>
2019-01-25 14:47:31 +0800

    删除所有文件从新写过程序

--- datashow/admin.py
+++
@@ -1,8 +0,0 @@
-# -*- coding:utf-8 -*-
-from django.contrib import admin
-from datashow.models import Screlation
-# Register your models here.
-class ScrelationAadmin(admin.ModelAdmin):
-    ordering = ("serviceid",)
-
-admin.site.register(Screlation, ScrelationAadmin)
